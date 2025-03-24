d83e4c48b0a9d891a3be67a7e4bfac13c2e34ca6
saksham1895garg <gargsaksham1895@gmail.com>
2025-03-24 21:51:48 +0530

    Changed port to PORT

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const {requireAuth, checkUser} = require('./server/middlewares/authMiddleware');
 
 
 const app = express(); 
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DB_PORT, {
   ssl: true,
